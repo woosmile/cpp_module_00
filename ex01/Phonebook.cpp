50c1c68d16926467cd03bebda7c75911f5b62730
woosmile <wooseong1106@gmail.com>
2023-09-21 17:33:36 +0900

    ex01 separate cpp, ex02 done

---
+++ ex01/Phonebook.cpp
@@ -0,0 +1,53 @@
+#include "Phonebook.hpp"
+
+void	Phonebook::AddContact()
+{
+	contact[index].InputData();
+	index = (index + 1) % 8;
+	std::cin.clear();
+	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+}
+
+void	Phonebook::ShowContact()
+{
+	int	find_idx;
+	int	show_idx;
+
+	show_idx = 0;
+	std::cout << std::setw(10) << "index";
+	std::cout << "|";
+	std::cout << std::setw(10) << "last name";
+	std::cout << "|";
+	std::cout << std::setw(10) << "first name";
+	std::cout << "|";
+	std::cout << std::setw(10) << "number" << std::endl;
+	while (contact[show_idx].CheckEmpty() != 1 && show_idx < 8)
+	{
+		contact[show_idx].ShowData(show_idx);
+		show_idx++;
+	}
+	while (1)
+	{
+		std::cout << "Enter an index for the contact: ";
+		std::cin.clear();
+		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		std::cin >> find_idx;
+		if (std::cin.eof())
+			exit(0);
+		if (std::cin.fail() || find_idx < 0 || find_idx > 8)
+		{
+			std::cout << "Invalid index number. Please enter between 0 and 7.\n";
+			continue ;
+		}
+		else
+		{
+			if (contact[find_idx].CheckEmpty())
+				std::cout << "This contact is empty.\n";
+			else
+				contact[find_idx].ShowData(find_idx);
+			break ;
+		}
+	}
+	std::cin.clear();
+	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+}
